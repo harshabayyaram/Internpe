d6b36ff562f6fde85a7716848276ef3e7f209351
bayyaramharsha <bayyaramharsha@gmail.com>
2023-06-08 19:24:21 +0530

    task 2 completed

---
+++ Java/src/com/harsha/interpe2/Task4.java
@@ -0,0 +1,17 @@
+package com.harsha.interpe2;
+
+public class Task4 {
+
+	public static void main(String[] args) {
+		String s1 = "* * * * * * ================================== \n * * * * *  ==================================";
+		
+		for(int i=0;i<4;i++){
+			System.out.println(s1);
+		}
+		System.out.println("* * * * * * ==================================");
+		for(int i = 0;i<6;i++){
+			System.out.println("==============================================");
+		}
+	}
+
+}
